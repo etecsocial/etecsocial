539f7877834b9c18739b70e4aa826a687a3e3908
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-02-26 22:53:08 -0300

    2016 :)

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer" >
     <div class="footer-copyright red lighten-2">
         <div class="container">
-            Copyright © 2015 <a class="grey-text text-lighten-4" href="http://www.etecsocial.com.br" target="_blank">ETEC Social</a> Todos os direitos Reservados.
+            Copyright © {{ date('Y') }} <a class="grey-text text-lighten-4" href="http://www.etecsocial.com.br">ETEC Social</a> Todos os direitos Reservados.
             <span class="right"> Saiba mais sobre o projeto em <a class="grey-text text-lighten-4" href="http://projeto.etecsocial.com">Projeto ETEC Social</a></span>
         </div>
     </div>
