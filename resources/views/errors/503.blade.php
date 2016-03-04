e7712b81f921f6c4adcad1c6d4ffde2a8a721659
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-04 20:56:30 -0300

    Errors views refeitos
    - Simplificado e otimizado

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,26 +1,30 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="description" content="A ETEC Social é a rede social da ETEC, nela será possivel compartilhar e adquirir conhecimento e se entreter junto a comunidade etequiana." /> 
-<meta name="author" content="Antonio Carlos, Beatriz Volpone, Gustavo Salles, Jhonatan Lopes, Marcio Simões, Matheus Gomide" />
-<meta name="robots" content="index,follow" />
-<meta name="googlebot" content="index,follow" />
-<meta name="rating" content="general" />
-<meta name="keywords" content="ETEC Social, ETEC, Escola Técnica, Rede Social, São Paulo, Centro Paula Souza, CPS, Conhecimento, Compartilhar, Amigos, Escola ETEC" />
-<meta property="og:site_name" content="ETEC Social">
-<meta property="og:title" content="ETEC Social - Entre, conheça ou cadastre-se.">
-<title>ETEC Social</title>
-<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-<style>body{margin:0;padding:0;width:100%;height:100%;color:#B0BEC5;display:table;font-weight:100;font-family:'Lato'}.container{text-align:center;display:table-cell;vertical-align:middle}.content{text-align:center;display:inline-block}.title{font-size:72px;margin-bottom:40px}</style>
-</head>
-<body>
-    <div class="container">
-        <div class="content">
-            <div class="title">Volte mais tarde!</div>
-            <h2>Estamos em manutenção no momento.</h2>
+@extends('app')
+@section('title')
+503 | Manutenção
+@stop
+
+@section('style')
+{!! Html::style('css/materialize.min.css') !!}
+@stop
+
+@section('content')
+<nav class="red darken-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">ETEC Social</a></div>
+</nav>
+
+<div id="index">
+    <div class="no-pad-bot">
+        <div class="container">
+            <br><br>
+            <h1 class="header center red-text text-darken-2">ERRO 503</h1>
+            <div class="row center ">
+                <h5 class="header col s12 red-text">Estamos em manutenção!</h5>
+                <h6 class="header col s12 red-text">Ou o estágiário está trabalhando :/</h6>
+            </div>
+            <div class="row center">
+                <a href="{{ url('/') }}" id="entrar-button" class="btn-large waves-effect waves-light red lighten-1 modal-trigger">Voltar</a>
+            </div>
         </div>
     </div>
-</body>
-</html>
+</div>
+@stop
