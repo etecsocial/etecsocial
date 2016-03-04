aa18105fe2bb380b66360a9d8b3302b7a620c9e0
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-03 22:22:39 -0300

    Install Intervetion Image
    Será útil para imagens, para redimencionar e manipular elas
    
    * required: extension=php_fileinfo.dll

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class
 
     ],
 
@@ -207,6 +208,7 @@ return [
         'Html'      => Collective\Html\HtmlFacade::class,
         'Input'     => Illuminate\Support\Facades\Input::class,
         'Socialize' => Laravel\Socialite\Facades\Socialite::class,
+        'Image'     => Intervention\Image\Facades\Image::class,
     ],
 
 ];
