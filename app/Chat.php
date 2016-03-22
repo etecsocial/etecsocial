b092211b5154ba15ef17d64a4565175b9e8b735c
Jhonatan <jhonatanlopes98@outlook.com>
2016-03-22 08:06:41 -0300

    Controlador de Mensagens em andamento...

--- app/Chat.php
+++ app/Chat.php
@@ -3,13 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Auth;
-
 use App\Amizade;
 
-class Chat extends Model
-{
+class Chat extends Model {
+
     protected $fillable = [
         'id_remetente',
         'id_destinatario',
@@ -18,41 +16,33 @@ class Chat extends Model
         'data',
         'visto'
     ];
-    
-    public static function loadMsgs($id_user) 
-    {
-        return Chat::where([ "id_remetente" => $id_user, "id_destinatario" => Auth::user()->id ])
-                ->orWhere([ "id_remetente" => Auth::user()->id, "id_destinatario" => $id_user ])
-                ->limit(15)
-                ->get();
+
+    public static function loadMsgs($id_user) {
+        return Chat::where([ "id_remetente" => $id_user, "id_destinatario" => Auth::user()->id])
+                        ->orWhere([ "id_remetente" => Auth::user()->id, "id_destinatario" => $id_user])
+                        ->limit(15)
+                        ->get();
     }
-    
-    public static function count() 
-    {
-        return Chat::where([ "id_destinatario" => Auth::user()->id, "visto" => 0 ])
-                ->count();
+
+    public static function count() {
+        return Chat::where([ "id_destinatario" => Auth::user()->id, "visto" => 0])
+                        ->count();
     }
- 
-    public static function loadUsers($on = true) 
-    {
+
+    public static function loadUsers($on = true) {
         return Amizade::where('id_user1', Auth::user()->id)
-                            ->where('aceitou', 1)
-                            ->where('online', $on)
-                            ->join('users', 'users.id', '=', 'amizades.id_user2')
-                            ->get();
+                        ->where('aceitou', 1)
+                        ->where('online', $on)
+                        ->join('users', 'users.id', '=', 'amizades.id_user2')
+                        ->get();
     }
-    
-    public static function lastMsg($id_user) 
-    {
-        $chat = Chat::where([ "id_remetente" => $id_user, "id_destinatario" => Auth::user()->id ])
-                 ->orWhere([ "id_remetente" => Auth::user()->id, "id_destinatario" => $id_user ])
+
+    public static function lastMsg($id_user) {
+        $chat = Chat::where([ "id_remetente" => $id_user, "id_destinatario" => Auth::user()->id])
+                ->orWhere([ "id_remetente" => Auth::user()->id, "id_destinatario" => $id_user])
                 ->orderBy('data', 'desc')
                 ->first();
-        
-        if ($chat) {
-            return $chat;
-        } else {
-            return false;
-        }
+        return isset($chat) ? $chat : false;
     }
+
 }
---
+++ app/Http/Controllers/MensagemController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Chat;
+
+/**
+ * Classe controladora de mensagens
+ *
+ * @2016 ETEC Social
+ */
+class MensagemController extends Controller {
+
+    public $extensionImg = ['jpg', 'JPG', 'png', 'PNG', 'gif'];
+    public $extensionVideos = ['flv', 'FLV', 'mp4', 'MP4', 'avi'];
+    public $extensionDocs = ['pdf', 'txt', 'doc', 'docx', 'pptx', 'xls'];
+    public $ImgDestinationPath = 'midia/imagens/chats';
+    public $VideoDestinationPath = 'midia/videos/chats';
+    public $DocsDestinationPath = 'docs/chats';
+
+    public function store(Request $request) {
+        if ($request->msg) {
+            Chat::create([
+                'id_remetente' => Auth::user()->id,
+                'id_destinatario' => $request->id_dest,
+                'msg' => $request->msg
+            ]);
+            return Response::json([ 'status' => true]); //RETORNAR JUNTO O ID GERADO PARA CONTROLE DE JS!!
+        }return Response::json([ 'status' => false]);
+    }
+
+    public function destroy(Request $request) {
+        if ($msg = Chat::where('id', $request->id)->first()) {
+            if (($msg->copia_dest == 0) and ( $request->is_rem) or ( ($msg->copia_rem == 0) and ( $request->is_dest))) {
+                $msg->delete();
+                return Response::json([ 'status' => true]);
+            }
+            $request->is_dest ? $msg->dest = 0 : $msg->rem = 0;
+            $msg->save();
+            return Response::json([ 'status' => true]);
+        }return Response::json([ 'status' => false]); //Menságem não existe mais!
+    }
+
+    public function setMidia(Request $request) {
+        if (($request->img) || ($request->doc) || ($request->video)) {
+            $msg = new Chat;
+            $msg->id_remetente = Auth::user()->id;
+            $msg->id_destinatario = $request->id_dest;
+            if (Input::hasFile('img')) {
+                $ext = Input::file('img')->getClientOriginalExtension();
+                if (in_array($ext, $this->extensionImg)) {
+                    Input::file('img')->move($this->ImgDestinationPath, md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext);
+                    $msg->img = $this->ImgDestinationPath . '/' . md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext;
+                } else {
+                    return Response::json([ 'response' => 4]); //Não é imagem válidda!!
+                }
+            } elseif (Input::hasFile('doc')) {
+                $ext = Input::file('doc')->getClientOriginalExtension();
+                if (in_array($ext, $this->extensionDocs)) {
+                    Input::file('doc')->move($this->DocsDestinationPath, md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext);
+                    $msg->doc = $this->DocsDestinationPath . '/' . md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext;
+                } else {
+                    return Response::json([ 'response' => 4]); //Não é documento válido!!
+                }
+            } elseif (Input::hasFile('video')) {
+                $ext = Input::file('video')->getClientOriginalExtension();
+                if (in_array($ext, $this->extensionVideos)) {
+                    Input::file('video')->move($this->VideoDestinationPath, md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext);
+                    $msg->video = $this->VideoDestinationPath . '/' . md5(Auth::User()->id . \Carbon\Carbon::now()) . '.' . $ext;
+                } else {
+                    return Response::json([ 'response' => 4]); //Não é documento válido!!
+                }
+            }
+            return $msg->save() ? Response::json([ 'status' => true]) : Response::json([ 'status' => false]);
+        }return Response::json([ 'response' => 3]); //Não é nenhuma fucking mídia!
+    }
+
+}
