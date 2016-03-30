f615e4ca654b91a537523ccd5fa39503a8d73a67
Jhonatan <jhonatanlopes98@outlook.com>
2016-03-30 11:16:55 -0300

    Correção bug

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,10 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
 
-class Authenticate {
-
+class Authenticate
+{
     /**
      * Handle an incoming request.
      *
@@ -16,22 +15,18 @@ class Authenticate {
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null) {
+    public function handle($request, Closure $next, $guard = null)
+    {
         if (auth()->guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
                 return redirect()->guest('login');
             }
-        } else {
-            Carbon::setLocale('pt_BR');
-
-            App::singleton('Auth', function($app) {
-                return Response::json([2]);
-            });
+        }else{
+            App\Carbon::setLocale('pt_BR');
         }
 
         return $next($request);
     }
-
 }
