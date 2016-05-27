69ed2a0f5ba1aa7779f13013c5dfbfc425ea9960
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-05-26 23:02:27 -0300

    missing ID no Turmas seed

--- database/seeds/Turmas.php
+++ database/seeds/Turmas.php
@@ -10,78 +10,91 @@ class Turmas extends Seeder
     public $turmas = [
 
         [
+            'id'        => 1,
             'nome'      => '1 Ensino Médio Integrado a Informática para Internet',
             'sigla'     => '1 EMIA',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 2,
             'nome'      => '2 Ensino Médio Integrado a Informática para Internet',
             'sigla'     => '2 EMIA',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 3,
             'nome'      => '3 Ensino Médio Integrado a Informática para Internet',
             'sigla'     => '3 EMIA',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 4,
             'nome'      => '1 Ensino Médio Integrado a Administração',
             'sigla'     => '1 EMAD',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 5,
             'nome'      => '2 Ensino Médio Integrado a Administração',
             'sigla'     => '2 EMAD',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 6,
             'nome'      => '3 Ensino Médio Integrado a Administração',
             'sigla'     => '3 EMAD',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 7,
             'nome'      => '2 Ensino Médio A',
             'sigla'     => '2 EMA',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 8,
             'nome'      => '3 Ensino Médio A',
             'sigla'     => '3 EMA',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 9,
             'nome'      => '2 Ensino Médio B',
             'sigla'     => '2 EMB',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 10,
             'nome'      => '3 Ensino Médio B',
             'sigla'     => '3 EMB',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 11,
             'nome'      => '1 Ensino Médio Integrado ao Meio Ambiente',
             'sigla'     => '1 EMMEIO',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 12,
             'nome'      => '2 Ensino Médio Integrado ao Meio Ambiente',
             'sigla'     => '2 EMMEIO',
             'id_escola' => 1,
         ],
 
         [
+            'id'        => 13,
             'nome'      => '3 Ensino Médio Integrado ao Meio Ambiente',
             'sigla'     => '3 EMMEIO',
             'id_escola' => 1,
