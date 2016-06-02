2778dcea49f2db8e20983da2f5871c7c20ff3084
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-06-02 10:32:39 -0300

    email config

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'contato@etecsocial.com.br', 'name' => 'ETEC Social'],
 
     /*
     |--------------------------------------------------------------------------
