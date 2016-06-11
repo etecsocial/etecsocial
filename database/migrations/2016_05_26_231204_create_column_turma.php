4f0eaf1ce92fa995c59b0796ebed68d60a1149b4
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-06-11 16:48:51 -0300

    fix migration

--- database/migrations/2016_05_26_231204_create_column_turma.php
+++ database/migrations/2016_05_26_231204_create_column_turma.php
@@ -14,7 +14,7 @@ class CreateColumnTurma extends Migration
         Schema::table('grupo', function ($table) {
             if (!Schema::hasColumn('grupo', 'id_turma')) {
                 $table->integer('id_turma')->unsigned()->nullable();
-                $table->foreign('id_turma')->references('id')->on('turmas');
+                $table->foreign('id_turma')->references('id')->on('turmas')->onDelete('cascade');
             }
         });
     }
@@ -28,7 +28,7 @@ class CreateColumnTurma extends Migration
     {
         Schema::table('grupo', function ($table) {
             if (Schema::hasColumn('grupo', 'id_turma')) {
-                $table->dropColumn('id_turma');
+                $table->dropForeign(['id_turma']);
             }
         });
     }
