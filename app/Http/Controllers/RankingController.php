cdca0fc300886d5325684e97d1c72f739883ed65
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-07 22:49:14 -0300

    Ranking refatorado
    - Model corrigido
    - Feito relações
    - View corrigido
    - Falta a lógica do professor turma, se ele pode ou não ver o ranking de
    cada turma

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -2,30 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use App\Pontuacao;
+use App\Turma;
+use DB;
 
 class RankingController extends Controller
 {
-  public function geral()
-  {
-      $usuarios = Pontuacao::ranking();
+    public function geral()
+    {
+        $usuarios = Pontuacao::selectRaw('sum(pontos) as pontos, pontuacaos.user_id')
+                          ->orderBy('pontos', 'DESC')
+                          ->limit(100)
+                          ->get();
 
-      return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking de todas as ETEC']);
-  }
+        return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking de todas as ETEC']);
+    }
 
-  public function etec()
-  {
-      $usuarios = Pontuacao::ranking('etec');
+    public function etec()
+    {
+        if(auth()->user()->turma != null){
+            $turma_id = auth()->user()->turma->turma_id;
+            $escola_id = Turma::select('escola_id')->where('id', $turma_id)->first()->escola_id;
+        } else {
+            $escola_id = auth()->user()->profInfo->escola_id;
+        }
 
-      return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking de toda a sua ETEC']);
-  }
+        DB::statement(DB::raw('set @position:=0')); // numero da posição
 
-  public function turma()
-  {
-      $usuarios = Pontuacao::ranking('turma');
+        $usuarios = Pontuacao::selectRaw('@position:=@position+1 as position, sum(pontos) as pontos, pontuacaos.user_id')
+                          ->join('alunos_turma', 'pontuacaos.user_id', '=', 'alunos_turma.user_id')
+                          ->join('turmas', 'alunos_turma.turma_id', '=', 'turmas.id')
+                          ->orderBy('pontos', 'DESC')
+                          ->where('turmas.escola_id', $escola_id)
+                          ->limit(100)
+                          ->get();
 
-      return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking da sua turma']);
-  }
+        return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking de toda a sua ETEC']);
+    }
+
+    public function turma()
+    {
+        if(auth()->user()->turma != null){
+          $turma_id = auth()->user()->turma->turma_id;
+        } else {
+          return "Não é possível visualizar turma por turma, veja o ranking geral de sua escola";
+        }
+        DB::statement(DB::raw('set @position:=0')); // numero da posição
+
+        $usuarios = Pontuacao::selectRaw('@position:=@position+1 as position, sum(pontos) as pontos, pontuacaos.user_id')
+                          ->join('alunos_turma', 'pontuacaos.user_id', '=', 'alunos_turma.user_id')
+                          ->orderBy('pontos', 'DESC')
+                          ->where('alunos_turma.turma_id', $turma_id)
+                          ->limit(50)
+                          ->get();
+
+        return view('desafio.ranking', ['usuarios' => $usuarios, 'tipo' => 'Ranking da sua turma']);
+    }
 }
--- resources/views/desafio/ranking.blade.php
+++ resources/views/desafio/ranking.blade.php
@@ -1,66 +1,60 @@
 @extends('base')
-
 @section('title')
 Ranking | ETEC Social
 @stop
-
 @section('style')
 {!! Minify::stylesheet(['/css/style.css'])->withFullURL() !!}
 <style> .ranking li { padding: 25px 0; } </style>
 @stop
-
 @section('jscript')
-{!! Html::script('js/jquery-1.11.2.min.js') !!}
-{!! Html::script('js/plugins/lightbox-plus-jquery.min.js') !!}
-{!! Html::script('js/materialize.js') !!}
-{!! Html::script('js/form.min.js') !!}
-{!! Html::script('js/plugins/jquery.nanoscroller.min.js') !!}
-{!! Html::script('js/plugins/sparkline/jquery.sparkline.min.js') !!}
-{!! Html::script('js/plugins/sparkline/sparkline-script.js') !!}
-{!! Html::script('js/plugins/succinct-master/jQuery.succinct.min.js') !!}
-
-{!! Html::script('js/script.js') !!}
-{!! Html::script('js/plugins.js') !!}
-
+{!! Minify::javascript(['/js/jquery-1.11.2.min.js',
+'/js/plugins/lightbox-plus-jquery.min.js',
+'/materialize-css/js/materialize.min.js',
+'/js/plugins/jquery.nanoscroller.min.js',
+'/js/plugins/sparkline/jquery.sparkline.min.js',
+'/js/plugins/sparkline/sparkline-script.js',
+'/js/plugins/succinct-master/jQuery.succinct.min.js',
+'/js/script.js',
+'/js/plugins.js']) !!}
 @stop
 @section('content')
 @include('partials._nav')
 <div id="breadcrumbs-wrapper" class="grey lighten-3">
-    <div class="container">
-        <div class="row">
-            <div class="col s12 m12 l12">
-                <h5 class="breadcrumbs-title">{{ $tipo }}</h5>
-                <ol class="breadcrumb">
-                    <li><a href="{{ url('/') }}">Pagina Inicial</a></li>
-                    <li><a href="{{ url('/desafios') }}">Desafios</a></li>
-                    <li><a href="{{ url('/ranking') }}">Ranking</a></li>
-                    <li class="active">{{ $tipo }}</li>
-                </ol>
-            </div>
-        </div>
-    </div>
+   <div class="container">
+      <div class="row">
+         <div class="col s12 m12 l12">
+            <h5 class="breadcrumbs-title">{{ $tipo }}</h5>
+            <ol class="breadcrumb">
+               <li><a href="{{ url('/') }}">Pagina Inicial</a></li>
+               <li><a href="{{ url('/desafios') }}">Desafios</a></li>
+               <li><a href="{{ url('/ranking') }}">Ranking</a></li>
+               <li class="active">{{ $tipo }}</li>
+            </ol>
+         </div>
+      </div>
+   </div>
 </div>
 <div class="container">
-    <p class="caption">Com o sistema de Reputação, alunos poderão desafiar seus colegas afim de adiquirir pontuação. Tais desafios consistirão tanto em questões de vestibular quanto outros tipos, que serão sugeridos pelo sistema.</p>
-    <div class="divider"></div>
-    <ul class="ranking">
-        @foreach($usuarios as $usuario)
-        <li class="row">
-            <div class="col l2">
-                <a href="{{ auth()->user()->avatar($usuario->user_id) }}" data-lightbox="ju">
-                    <img src="{{ auth()->user()->avatar($usuario->user_id) }}" alt="avatar" class="circle responsive-img valign profile-image">
-                </a>
-            </div>
-            <div class="col l5">
-                #1
-                <a href="{{ url($usuario->username) }}">
-                    <h5>{{$usuario->name}}</h5>
-                </a>
-                <h6>{{$usuario->pontos}} pontos</h6>
-            </div>
-        </li>
-        <div class="divider"></div>
-        @endforeach
-    </ul>
+   <p class="caption">É possível conseguir pontos através de desafios feitos pelos professores! <a href="{{url('/desafios')}}">Desafios</a></p>
+   <div class="divider"></div>
+   <ul class="ranking">
+      @foreach($usuarios as $usuario)
+      <li class="row">
+         <div class="col l2">
+            <a href="{{ auth()->user()->avatar($usuario->user_id) }}" data-lightbox="ju">
+            <img src="{{ auth()->user()->avatar($usuario->user_id) }}" alt="avatar" class="circle responsive-img valign profile-image">
+            </a>
+         </div>
+         <div class="col l5">
+            # {{ $usuario->position }}
+            <a href="{{ url($usuario->user->username) }}">
+               <h5>{{$usuario->user->name}}</h5>
+            </a>
+            <h6>{{$usuario->pontos}} pontos</h6>
+         </div>
+      </li>
+      <div class="divider"></div>
+      @endforeach
+   </ul>
 </div>
 @stop
