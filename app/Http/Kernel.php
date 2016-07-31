781178fc9dcb5732f082e4683f183bb15935fe2f
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-31 11:34:03 -0300

    Alterando a posição dos middlewares
    - Isso evita problemas de sessão em páginas que não tem o middleware web

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+      \App\Http\Middleware\EncryptCookies::class,
+      \Illuminate\Session\Middleware\StartSession::class,
+      \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
     /**
@@ -23,15 +25,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        
+
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
            // \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\HTMLminify::class,
-            
+
         ],
 
         'api' => [
@@ -51,6 +51,6 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        
+
     ];
 }
