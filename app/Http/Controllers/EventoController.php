7caecedcd8ac32a0f6472dafc5b7954716483838
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-31 12:36:29 -0300

    Pequena correção no Evento Controller
    - Não está funcionando, precisa criar novos models para cada evento:
    - EscolaEvento
    - TurmaEvento
    - UserEvento

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -5,28 +5,27 @@ namespace App\Http\Controllers;
 use App\Evento;
 use App\Grupo;
 use App\GrupoUser;
-use App\Http\Controllers\Controller;
-use App\Mensagens;
+
 use App\Notificacao;
 use App\User;
-use DB;
 use Illuminate\Http\Request;
 use Input;
 use Response;
 
-class EventoController extends Controller {
-
+class EventoController extends Controller
+{
     /**
      * Display a listing of the resource.
      *
      * @return Response
      */
-    public function index() {
+    public function index()
+    {
         return view('agenda.home');
     }
 
-    public function show(Request $request) {
-
+    public function show(Request $request)
+    {
         $evento = Evento::select(['eventos.id', 'user_id', 'is_publico', 'start', 'end', 'title', 'description', 'users.name'])
                 ->where(function ($query) use ($request) {
                     $query->where('start', '>=', $request->start)
@@ -34,8 +33,7 @@ class EventoController extends Controller {
                 })
                 ->where(function ($query) {
                     $query->orWhere(function ($query) {
-                        $db = DB::table('alunos_info')->select('turma_id')->where('user_id', auth()->user()->id)->first();
-                        $turma_id = $db->turma_id;
+                        $turma_id = auth()->user()->turma->turma_id;
                         $query->where('eventos.turma_id', $turma_id)
                         ->where('is_publico', 1);
                     })
@@ -52,8 +50,8 @@ class EventoController extends Controller {
      *
      * @return Response
      */
-    public function create() {
-        
+    public function create()
+    {
     }
 
     /**
@@ -61,22 +59,22 @@ class EventoController extends Controller {
      *
      * @return Response
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $this->validate($request, ['title' => 'required']);
 
         if (auth()->user()->type == 1) {
-            $data = $request->start ? $request->start : date("Y-m-d");
+            $data = $request->start ? $request->start : date('Y-m-d');
 
             Notificacao::create([
                 'rem_id' => auth()->user()->id,
                 'id_dest' => auth()->user()->id,
                 'data' => strtotime($data) - 3 * 24 * 60 * 60,
-                'texto' => 'O evento "' . $request->title . '" está próximo.',
+                'texto' => 'O evento "'.$request->title.'" está próximo.',
                 'is_post' => false,
             ]);
 
-            $db = DB::table('alunos_info')->select('turma_id')->where('user_id', auth()->user()->id)->first();
-            $turma_id = $db->turma_id;
+            $turma_id = auth()->user()->turma->turma_id;
 
             return Evento::create([
                         'title' => $request->title,
@@ -90,9 +88,10 @@ class EventoController extends Controller {
         } else {
             $request->turma ? $this->CreateGrupoByTurma($request->turma, $request->title, $request->start, $request->end, $request->description) : null;
             $request->turma_id ? $this->CreateGrupoByTurma($request->turma_id, $request->title, $request->start, $request->end, $request->description) : null;
+
             return Evento::create([
                         'title' => $request->title,
-                        'start' => $request->start ? $request->start : date("Y-m-d"),
+                        'start' => $request->start ? $request->start : date('Y-m-d'),
                         'end' => $request->end ? $request->end : $request->start,
                         'user_id' => auth()->user()->id,
                         'is_publico' => $request->publico,
@@ -105,28 +104,27 @@ class EventoController extends Controller {
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-    public function makeUrl($nome) {
+    public function makeUrl($nome)
+    {
         $url = str_replace(' ', '', $nome);
         $cont = 1;
         while (Grupo::where('url', $url)->select('id')->first()) {
             //falta deixar usar url de grupo expirado
-            $url = $url . $cont;
-            $cont++;
-        }return $url;
+            $url = $url.$cont;
+            ++$cont;
+        }
+
+        return $url;
     }
 
-    public function CreateGrupoByTurma($turma_id, $titulo, $start, $end, $desc) {
-
-
+    public function CreateGrupoByTurma($turma_id, $titulo, $start, $end, $desc)
+    {
 
         //Automatizar isso, usar o service provider!!
-
-
-
-
 
         if (Grupo::create([
                     'nome' => $titulo,
@@ -134,11 +132,11 @@ class EventoController extends Controller {
                     'url' => $this->makeUrl($titulo),
                     'id_criador' => auth()->user()->id,
                     'criacao' => Carbon::today(),
-                    'expiracao' => isset($end) ? $end : $start
+                    'expiracao' => isset($end) ? $end : $start,
                 ])) {
             $alunos = User::where('turma_id', $turma_id)->select('id')->where('id', '<>', auth()->user()->id)->get();
             foreach ($alunos as $aluno) {
-                $grupoUser = new GrupoUser;
+                $grupoUser = new GrupoUser();
                 $grupoUser->grupo_id = $grupo->id;
                 $grupoUser->user_id = $aluno->id;
                 $grupoUser->is_admin = 0;
@@ -147,11 +145,13 @@ class EventoController extends Controller {
                     'rem_id' => auth()->user()->id,
                     'id_dest' => $aluno->id,
                     'data' => time(),
-                    'texto' => 'Adicionou você ao grupo "' . Grupo::verGrupo($grupo->id)->url . '"',
+                    'texto' => 'Adicionou você ao grupo "'.Grupo::verGrupo($grupo->id)->url.'"',
                     'is_post' => false,
-                    'action' => '/grupo/' . Grupo::verGrupo($grupo->id)->url,
+                    'action' => '/grupo/'.Grupo::verGrupo($grupo->id)->url,
                 ]);
-            }return true;
+            }
+
+            return true;
         } else {
             return false;
         }
@@ -160,10 +160,12 @@ class EventoController extends Controller {
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-    public function edit($id) {
+    public function edit($id)
+    {
         $evento = Evento::where('id', $id)->limit(1)->first();
 
         $start = Input::has('start') ? Input::get('start') : $evento->start;
@@ -183,20 +185,24 @@ class EventoController extends Controller {
     /**
      * Update the specified resource in storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-    public function update($id) {
+    public function update($id)
+    {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $evento = Evento::where('id', $id)->limit(1)->first();
 
         if (auth()->user()->id !== $evento->user_id) {
@@ -207,5 +213,4 @@ class EventoController extends Controller {
 
         return Response::json(['status' => true]);
     }
-
 }
